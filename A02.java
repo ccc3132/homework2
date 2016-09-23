a22e26df40fb448a9f1b47566d3278d4f40b6aa1
최성근 <ccc3132@naver.com>
2016-09-23 22:44:30 +0900

    추가

--- A02.java
+++ A02.java
@@ -1,32 +1,40 @@
 import java.util.*;
-class A01{
+class A02{
 
-		int age = 0;
-
+		double hwa= 0;
+		double sub=0;
+		
 
            public static void main(String[] args)
 	   {
-	          new A01().start(); 
+	          new A02().start(); 
 	   }
 
 	   void start(){
 	          Scanner s = new Scanner(System.in);
-	          System.out.print("나이를 입력:");
+	          System.out.print("온도를 입력하세요");
 
-		  int year = s.nextInt();
+		  double input_degree = s.nextInt();
 
-	   	this.age = 2016 - year +1;
-	   	print();
+	   	this.hwa = sub * 1.8 + 32;
+		this.sub = (hwa -32)/1.8;
+                 System.out.print("입력하신 온도가 섭씨온도이면 C를 화씨온도이면 F를 입력하세요.");
+		 String kind = s.next();
+		print();
 	   }
 
 
 	   void print(){
 	   
 	
-	        if(age < 20)
-			System.out.println("미성년자입니다.");
+	        if(kind =='C')
+			sub = input_degree;
+		else if(kind =='F')
+		        hwa = input_degree;
 		else
-			System.out.println("미성년자가 아닙니다.");
+		print("이건 온도가 아닙니다.");
+	
+			
 	   
 	   }
 
